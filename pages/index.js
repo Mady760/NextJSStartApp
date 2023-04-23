e0305393b2b5e9135d7ce5e8b906cfe78cdac83f
Mady <userfree761@gmail.com>
2023-04-23 03:18:00 -0700

    bca

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>
-        <h1>Hello Worl</h1>
+        <h1>Hello World</h1>
       </main>
     </>
   )
